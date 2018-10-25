897da9e3609e5c0adf9bd7d382156e05a8488240
bmadhu199 <b.madhu199@gmail.com>
2018-10-25 20:31:59 +0530

    basics

--- basicsWithConcepts/leapYear.py
+++ basicsWithConcepts/leapYear.py
@@ -1,9 +1,37 @@
-year = input("Enter a number")
+repete = True
 
-leap = False
+while(repete == True):
 
-if (year % 4) == 0 :
-    print(leap)
-elif (year % 100) == 0:
+    year = int(input("Enter a number"))
 
-    print(leap)
+    leap = False
+
+    if (year % 4 ) == 0 :
+        if(year % 100 ) == 0 :
+            if(year % 400 ) == 0 :
+             print("its a leap year ")
+            else :
+             print("Its not a leap year")
+        else:
+         print("Its a leap year")
+    else :
+     print("its not a leap year")
+
+
+    print("********************************************")
+
+
+    rep = input("wish to try again ?????   Y or N")
+
+
+    if(rep == 'y' or rep == 'Y'):
+            repete = True
+    else:
+
+        repete = False
+
+
+print("Thanks for playing")
+
+
+
